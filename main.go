dc3cb917602ab4953657fe697eb4444d2f6424b7
Justin Forseth <justin@jforseth.com>
2024-03-18 22:09:44 -0600

    Confirmation, other improvements

--- main.go
+++ main.go
@@ -10,7 +10,6 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
-	"strconv"
 	"strings"
 
 	"fyne.io/fyne/v2"
@@ -29,7 +28,7 @@ func main() {
 		for _, uri := range uris {
 			uriStrings = append(uriStrings, uri.Path())
 		}
-		err := UploadMultipleFiles("http://10.21.20.202:8080", "file", uriStrings)
+		err := UploadMultipleFiles("http://10.21.20.202:8080", uriStrings)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -48,46 +47,57 @@ func main() {
 				http.Error(responseWriter, err.Error(), http.StatusInternalServerError)
 				return
 			}
-			files := []multipart.File{}
 			handlers := []*multipart.FileHeader{}
-			for i := 0; i < len(request.MultipartForm.File); i++ {
-				// Get the uploaded file
-				file, handler, err := request.FormFile("file" + strconv.Itoa(i))
+			requestFiles := request.MultipartForm.File["file"]
+			for _, handler := range requestFiles {
+
+				file, err := handler.Open()
 				if err != nil {
 					http.Error(responseWriter, err.Error(), http.StatusBadRequest)
 					return
 				}
-				files = append(files, file)
 				handlers = append(handlers, handler)
 				defer file.Close()
 			}
-			dialog.ShowFolderOpen(func(uris fyne.ListableURI, err error) {
-				if err != nil {
-					http.Error(responseWriter, err.Error(), http.StatusInternalServerError)
-					return
-				}
 
-				if uris == nil {
-					return
-				}
-				for i, file := range files {
-					// Create a new file in the selected directory with the same name as the uploaded file
-					emptyFile, err := os.Create(filepath.Join(uris.Path(), handlers[i].Filename))
-					if err != nil {
-						http.Error(responseWriter, err.Error(), http.StatusInternalServerError)
+			dialog.ShowConfirm("Accept Files",
+				"\""+getIPAsWords(net.ParseIP(strings.Split(request.RemoteAddr, ":")[0]))+"\" wants to send you these files:"+"\n"+strings.Join(getFileNames(handlers), "\n")+"\n Accept?", func(ok bool) {
+					if !ok {
 						return
 					}
-					defer file.Close()
+					dialog.ShowFolderOpen(func(uris fyne.ListableURI, err error) {
+						if err != nil {
+							http.Error(responseWriter, err.Error(), http.StatusInternalServerError)
+							return
+						}
 
-					// Copy the contents of the uploaded file to the newly created file
-					_, err = io.Copy(emptyFile, file)
-					if err != nil {
-						http.Error(responseWriter, err.Error(), http.StatusInternalServerError)
-						return
-					}
-					updateLabel <- fmt.Sprintf("File uploaded: %s", handlers[i].Filename)
-				}
-			}, window)
+						if uris == nil {
+							return
+						}
+						for i, handler := range handlers {
+							file, err := handler.Open()
+							if err != nil {
+								http.Error(responseWriter, err.Error(), http.StatusInternalServerError)
+								return
+							}
+							// Create a new file in the selected directory with the same name as the uploaded file
+							emptyFile, err := os.Create(filepath.Join(uris.Path(), handlers[i].Filename))
+							if err != nil {
+								http.Error(responseWriter, err.Error(), http.StatusInternalServerError)
+								return
+							}
+							defer file.Close()
+
+							// Copy the contents of the uploaded file to the newly created file
+							_, err = io.Copy(emptyFile, file)
+							if err != nil {
+								http.Error(responseWriter, err.Error(), http.StatusInternalServerError)
+								return
+							}
+							updateLabel <- fmt.Sprintf("File uploaded: %s", handlers[i].Filename)
+						}
+					}, window)
+				}, window)
 		})
 
 		http.ListenAndServe(":8080", nil)
@@ -120,6 +130,13 @@ func GetLocalIP() net.IP {
 var wordList = []string{
 	"time", "people", "year", "way", "thing", "man", "day", "life", "woman", "world", "child", "school", "state", "family", "president", "house", "student", "part", "place", "problem", "country", "week", "point", "hand", "group", "guy", "case", "question", "work", "night", "game", "number", "money", "lot", "book", "system", "government", "city", "company", "story", "job", "friend", "word", "fact", "right", "month", "program", "business", "home", "kind", "study", "issue", "name", "idea", "room", "percent", "law", "power", "kid", "war", "head", "mother", "team", "eye", "side", "water", "service", "area", "person", "end", "hour", "line", "girl", "father", "information", "car", "minute", "party", "back", "health", "reason", "member", "community", "news", "body", "level", "boy", "university", "change", "center", "face", "food", "history", "result", "morning", "parent", "office", "research", "door", "court", "moment", "street", "policy", "table", "care", "process", "teacher", "data", "death", "experience", "plan", "education", "age", "sense", "show", "college", "music", "mind", "class", "police", "use", "effect", "season", "tax", "heart", "son", "art", "market", "air", "force", "foot", "baby", "love", "republican", "interest", "security", "control", "rate", "report", "nation", "action", "wife", "decision", "value", "phone", "thanks", "event", "site", "church", "model", "relationship", "movie", "field", "player", "couple", "record", "difference", "light", "development", "role", "view", "price", "effort", "voice", "department", "leader", "photo", "space", "project", "position", "million", "film", "need", "type", "town", "article", "road", "form", "chance", "drug", "situation", "practice", "science", "brother", "matter", "image", "star", "cost", "post", "society", "picture", "piece", "paper", "energy", "building", "doctor", "activity", "american", "media", "evidence", "product", "arm", "technology", "comment", "look", "term", "color", "choice", "source", "mom", "director", "rule", "campaign", "ground", "election", "page", "test", "patient", "video", "support", "rest", "step", "opportunity", "official", "oil", "call", "organization", "character", "county", "future", "dad", "industry", "second", "list", "stuff", "figure", "attention", "risk", "fire", "dog", "hair", "condition", "wall", "daughter", "deal", "author", "truth", "husband", "period", "series", "order", "officer", "land", "computer", "thought", "economy"}
 
+func getFileNames(handlers []*multipart.FileHeader) []string {
+	var fileNames []string
+	for _, handler := range handlers {
+		fileNames = append(fileNames, handler.Filename)
+	}
+	return fileNames
+}
 func getIPAsWords(ip net.IP) string {
 	digits := ip.To4()
 	var digitStrings []string
@@ -130,16 +147,16 @@ func getIPAsWords(ip net.IP) string {
 	return strings.Join(digitStrings, " ")
 }
 
-func UploadMultipleFiles(url string, paramName string, filePaths []string) error {
+func UploadMultipleFiles(url string, filePaths []string) error {
 	body := &bytes.Buffer{}
 	writer := multipart.NewWriter(body)
-	for i, filePath := range filePaths {
+	for _, filePath := range filePaths {
 		file, err := os.Open(filePath)
 		if err != nil {
 			return err
 		}
 		defer file.Close()
-		part, err := writer.CreateFormFile("file"+strconv.Itoa(i), filepath.Base(filePath))
+		part, err := writer.CreateFormFile("file", filepath.Base(filePath))
 		if err != nil {
 			return err
 		}
